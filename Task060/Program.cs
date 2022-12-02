6aa82caeef84f2232c5537d4a5dbed91a38968d2
Vyugar <vyugar@yandex.ru>
2022-12-02 17:30:23 +0300

    aded Task060

---
+++ Task060/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы 
+каждого элемента. 
+
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+int Row = 2, Col = 2, array = 2;
+int FirstNumber = 10, SecondNumber = 100;
+int[] numbers = new int[SecondNumber - FirstNumber];
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = FirstNumber + i;
+}
+for (int i = 0; i < numbers.Length - 1; i++)
+{
+    int index = new Random().Next(i + 1, numbers.Length);
+    int temp = numbers[i];
+    numbers[i] = numbers[index];
+    numbers[index] = temp;
+}
+int[,,] TripleMatrix = new int[Row, Col, array];
+int count = 0;
+for (int i = 0; i < Row; i++)
+{
+    for (int j = 0; j < Col; j++)
+    {
+        for (int z = 0; z < array; z++)
+        {
+            TripleMatrix[i, j, z] = numbers[count];
+            Console.Write($" {TripleMatrix[i, j, z]}({i},{j},{z})");
+            count++;
+        }
+        Console.WriteLine();
+    }
+}
